62c642fbd245762a4703abd7b7708550712b9c90
Jon-Dev21 <jonacevedev96@gmail.com>
2021-11-19 15:57:14 -0400

    Added new script GameManager
    Currently, this script is used to keep track of the current score and the high score. It also has a method to increment the score by 1 and show it in the GUI

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// This script counts the score,
+/// </summary>
+public class GameManager : MonoBehaviour
+{
+    // Game score in integer form
+    public int score;
+
+    // Game score in text form. Used to show score in the GUI
+    public Text scoreText;
+
+
+    // Game highscore in text form. Used to show highscore in the GUI
+    public Text highScoreText;
+
+    /// <summary>
+    /// When the game starts, display the high score.
+    /// </summary>
+    private void Awake()
+    {
+        highScoreText.text = "HighScore: " + PlayerPrefs.GetInt("HighScore");
+    }
+
+    /// <summary>
+    /// This method increases the score and assigns it to our score text.
+    /// </summary>
+    public void IncreaseScore()
+    {
+        // Increment Score
+        score++;
+
+        // Assign score to score text in order to display it in the gui.
+        scoreText.text = "Score: "+score.ToString();
+        
+        
+
+        // If the score is greater than the highscore.
+        if (score > PlayerPrefs.GetInt("HighScore"))
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+            // Assign HighScore to highscore text in order to display it in the gui.
+            highScoreText.text = "HighScore: " + PlayerPrefs.GetInt("HighScore").ToString();
+        }
+        
+
+
+    }
+}
