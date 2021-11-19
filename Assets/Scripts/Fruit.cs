136c1804fae6a13b9eb3bb7428c4fb557b355b56
Jon-Dev21 <jonacevedev96@gmail.com>
2021-11-18 23:51:13 -0400

    Added code to fruit script in order to rotate and explode the cut pieces in random directions. Testing with space bar for now.

--- Assets/Scripts/Fruit.cs
+++ Assets/Scripts/Fruit.cs
@@ -11,6 +11,13 @@ public class Fruit : MonoBehaviour
     // Stores a prefab for the sliced fruit
     public GameObject slicedFruitPrefab;
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CreateSlicedFruit();
+        }
+    }
 
     /// <summary>
     /// Create a sliced fruit whenever a fruit is sliced.
@@ -18,7 +25,26 @@ public class Fruit : MonoBehaviour
     public void CreateSlicedFruit()
     {
         // Create a sliced fruit
-        Instantiate(slicedFruitPrefab, transform.position, transform.rotation);
+        GameObject inst = Instantiate(slicedFruitPrefab, transform.position, transform.rotation);
+
+        // Add a Rigid body that does an explotion.
+
+        // Using RigidBody Array in order to store the 2 sliced pieces of the fruit.
+        Rigidbody[] SlicedRigidBodies = inst.transform.GetComponentsInChildren<Rigidbody>();
+
+        // Iterate through the sliced fruit pieces.
+        foreach (Rigidbody fruitSlice in SlicedRigidBodies)
+        {
+            // Add a random rotation to the sliced piece.
+            fruitSlice.transform.rotation = Random.rotation;
+
+            // Add an explosion for the pieces to go to different locations.
+            // AddExplosionForce(explosionForce, explosionPosition, explosionRadius) 
+            // The fruit will explode with a force between 500 and 1000. The explosion
+            // Will appear in the position of the current fruit. The explosion radius will be 5.
+            fruitSlice.AddExplosionForce(Random.Range(500,1000), transform.position, 5);
+            
+        }
 
         // Destroy the unsliced fruit.
         Destroy(gameObject);
