e3d91f86976126f13b32d8530046367a5e75e686
Jon-Dev21 <jonacevedev96@gmail.com>
2021-11-19 16:35:32 -0400

    Created a new script for the Bomb
    Added script that detects if the blade cut the bomb.
    
    For now, if this happens, the game is stopped. Further game logic will be developed soon.

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This script handles when a bomb is hit by the blade.
+/// </summary>
+public class Bomb : MonoBehaviour
+{
+    /// <summary>
+    /// Executes whenever a collision is detected. (When the blade hits the bomb)
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Blade blade = collision.GetComponent<Blade>();
+
+        // If whatever hit the bomb was not the blade
+        if (!blade)
+        {
+            // Do nothing
+            return;
+        }
+
+        // Execute the OnBombHit method.
+        FindObjectOfType<GameManager>().OnBombHit();
+
+    }
+}
