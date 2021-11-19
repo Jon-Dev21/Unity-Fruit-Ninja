40e206f92ace74c0d82b3dd79247677910165248
Jon-Dev21 <jonacevedev96@gmail.com>
2021-11-19 13:51:38 -0400

    Created new spawner script to spawn fruits
    Script takes a Fruit prefab as well as the minimum spawn time, maximum spawn time, minimum force and maximum force to impulse the fruit out of the spawner objects.
    
    The script also contains a public property for a list of transform objects used to pass in the positions of the spawn places. (Fruits will spawned from the passed spawn places)
    
    This script is assigned to empty spawner objects in the main scene.

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Script used to spawn fruits from the bottom of the screen.
+/// </summary>
+public class Spawner : MonoBehaviour
+{
+    // Variable for the fruit to spawn.
+    public GameObject fruitToSpawn;
+
+    // List of our fruit spawners.
+    public Transform[] spawners;
+
+    // Minimum wait time for the fruit to spawn
+    public float minWait = .3f;
+
+    // Maximum wait time for the fruit to spawn
+    public float maxWait = 1f;
+
+    // Minimum force for the fruit to be spawned with
+    public float minForce = 10;
+
+    // Maximum force for the fruit to be spawned with
+    public float maxForce = 17;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnFruits());
+    }
+
+    private IEnumerator SpawnFruits()
+    {
+        // As long as this Coroutine runs, spawn fruits
+        while (true)
+        {
+            // If you want to do something after an amount of time,
+            // Use yield return
+            yield return new WaitForSeconds(Random.Range(minWait, maxWait));
+
+            // Assign the position of our fruit in a random position of the spawn places.
+            Transform t = spawners[Random.Range(0, spawners.Length)];
+
+            // Creating the fruit object
+            GameObject fruit = Instantiate(fruitToSpawn, t.position, t.rotation);
+
+            // Apply force to make the fruits fly to the air
+            // (Force comes from our spawners in order for the fruits to have a specific rotation)
+            fruit.GetComponent<Rigidbody2D>().AddForce(t.transform.up * Random.Range(minForce, maxForce), ForceMode2D.Impulse);
+
+            // Destroy fruits after 5 seconds
+            Destroy(fruit, 5);
+        }
+    }
+
+
+    
+}
