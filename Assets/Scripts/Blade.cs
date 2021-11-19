e35ad4838a1f174a10f67d606b4e950604d46ce9
Jon-Dev21 <jonacevedev96@gmail.com>
2021-11-19 16:42:45 -0400

    Added descriptive comment to the SetBladeToMouse method

--- Assets/Scripts/Blade.cs
+++ Assets/Scripts/Blade.cs
@@ -21,6 +21,9 @@ public class Blade : MonoBehaviour
         SetBladeToMouse();
     }
 
+    /// <summary>
+    /// Sets the blade position to the mouse's position.
+    /// </summary>
     private void SetBladeToMouse()
     {
         // Getting the mouse position.
