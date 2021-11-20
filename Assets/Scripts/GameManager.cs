c4a48a295879089066300479512328a56529ceda
Jon-Dev21 <jonacevedev96@gmail.com>
2021-11-19 21:32:57 -0400

    Multiple Changes to the GameManager script
    Created a method to restart the game.
    I originally intended to load the scene again but used another method which involves tagging the Interactable objects and destroying them with a foreach loop as well as resetting the values such as score, scoreText, etc.
    
    Imported the Scene Management package (might remove since another way to restart the game was implemented)
    
    Added Headers to the GameManager properties. These headers will show in the Inspector when adding game objects to the scripts.
    
    Added code to hide and show the game over panel whenever the game is over or is restarted.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,27 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 /// <summary>
 /// This script counts the score,
 /// </summary>
 public class GameManager : MonoBehaviour
 {
+    // This header will appear in the Inspector
+    [Header("Score Elements")]
     // Game score in integer form
     public int score;
 
     // Game score in text form. Used to show score in the GUI
     public Text scoreText;
 
-
     // Game highscore in text form. Used to show highscore in the GUI
     public Text highScoreText;
+
+    // This header will appear in the Inspector
+    [Header("Game Over")]
+    // Game Over Panel
+    public GameObject gameOverPanel;
+    // Score text in the game over panel
+    public Text gameOverScoreText;
 
     /// <summary>
     /// When the game starts, display the high score.
     /// </summary>
     private void Awake()
     {
+        // Deactivate the Game Over Panel when the game starts
+        gameOverPanel.SetActive(false);
+
+        // Assign the HighScore to the HighScore Text when the game starts
         highScoreText.text = "HighScore: " + PlayerPrefs.GetInt("HighScore");
     }
 
@@ -55,5 +68,35 @@ public class GameManager : MonoBehaviour
     public void OnBombHit()
     { 
         Time.timeScale = 0;                 // This line of code stops the game.
+
+        // Set the score to the gameOverScoreText
+        gameOverScoreText.text = "Score: " + score.ToString();
+
+        gameOverPanel.SetActive(true);     // Activate the Game Over Panel when the game ends
+    }
+
+    /// <summary>
+    /// This method restarts the game.
+    /// </summary>
+    public void RestartGame()
+    {
+        // One alternative
+        //Time.timeScale = 1;
+        //SceneManager.LoadScene(0);
+
+        // Another way to restart the game
+        Time.timeScale = 1;
+        score = 0;
+        scoreText.text = "Score: 0";
+
+        gameOverPanel.SetActive(false);
+        gameOverScoreText.text = "Score: 0";
+
+        // Destroy each game object
+        foreach (GameObject gameObj in GameObject.FindGameObjectsWithTag("Interactable"))
+        {
+            Destroy(gameObj);
+        }
+
     }
 }
