7c20738c694641841beebf3b6a03b40432dfcc7f
Jon-Dev21 <jonacevedev96@gmail.com>
2021-11-19 22:04:11 -0400

    Added code to prevent the blade from cutting whenever the mouse is not moving
    Created new method IsMouseMoving(). This boolean method returns true if the mouse is moving and returns false when it's not
    
    Added a few new properties in order to calculate values to determine if the mouse is moving or not.

--- Assets/Scripts/Blade.cs
+++ Assets/Scripts/Blade.cs
@@ -7,18 +7,44 @@ using UnityEngine;
 /// </summary>
 public class Blade : MonoBehaviour
 {
+    // Minimum velocity of the blade
+    public float minVelocity = 0.1f;
+
+    // A 3D vector of the last position of the mouse.
+    private Vector3 lastMousePosition;
+
+    // A 3D vector of the mouse velocity
+    private Vector3 mouseVelocity;
+
+    // A 2D collider. Should only be active when the mouse is moving
+    private Collider2D collider2d;
+
+    // Rigid body of the blade.
     private Rigidbody2D bladeRigidBody;
 
     private void Awake()
     {
         // Initialize the blade rigid body.
         bladeRigidBody = GetComponent<Rigidbody2D>();
+
+        // Initializing Collider
+        collider2d = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {    
         SetBladeToMouse();
+    }
+
+    /// <summary>
+    /// The code in this method prevents the fruit (or bomb)
+    /// from being cut whenever the mouse is not moving.
+    /// </summary>
+    private void FixedUpdate()
+    {
+        // Enable and disable the collider2d if the mouse is moving.
+        collider2d.enabled = IsMouseMoving();
     }
 
     /// <summary>
@@ -34,5 +60,38 @@ public class Blade : MonoBehaviour
         
         // Setting the blade position to the mouse position.
         bladeRigidBody.position = Camera.main.ScreenToWorldPoint(mousePos);
+    }
+
+    /// <summary>
+    /// Method that returns true if mouse is moving. 
+    /// Else, returns false
+    /// </summary>
+    /// <returns></returns>
+    private bool IsMouseMoving()
+    {
+        Vector3 currentMousePosition = transform.position;
+
+        // Calculate how much the mouse has traveled
+        float traveled = (lastMousePosition - currentMousePosition).magnitude;
+
+        // Set the last mouse position to the current mouse position
+        lastMousePosition = currentMousePosition;
+
+        //Debug.Log("Traveled: " + traveled);
+        //Debug.Log("MinVelocity: " + minVelocity);
+        // If the mouse has traveled enough distance, return true.
+        // Else, return false
+        // The distance would be our minimum velocity in this case.
+        if (traveled > minVelocity)
+        {
+            Debug.Log("True");
+            return true;
+        }   
+        else
+        {
+            Debug.Log("False");
+            return false;
+        }
+            
     }
 }
