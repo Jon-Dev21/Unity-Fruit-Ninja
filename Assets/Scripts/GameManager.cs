0f5fa87c4f78276553fd26b297c1d92c04e23655
Jon-Dev21 <jonacevedev96@gmail.com>
2021-11-20 00:31:07 -0400

    Multiple Updates to GameManager Script
    Added 4 new properties.
    
    - A public Array of Audio Clips for the slicing sounds.
    - A public audio clip for the Object spawn
    - A public audio clip for the bomb explosion
    - A private AudioSource to play all the audio clips with the PLayOneStop() method.
    
    Added code to add advertisements.
    Imported the UnityEngine.Advertisements package.
    However, this code is commented and will not be a part of the project in GitHub. (If you wish to experiment with adding ads, create your own unity account and go through the process of getting your personal GameId for the project.)
    
    Added methods to play the sounds from the passed audio clips. These include the spawn sound, slice sounds and explosion sound. Might add some background music later.
    
    (Important)
    Modified the OnBombHit method to show the GameOverPanel when the bomb explosion is heard.
    
    Had to research how to do it. Tried to use the Invoke method but found out that Unity recommends using a coroutine for this sort of stuff.
    
    Found out that the yield return new WaitForSeconds() wont work if the timescale is set to 0 so I set it to 0.01, multiplied that with 1.3 (which is the seconds it takes for the explosion to sound after the bomb-omb sound) and got 0.013 which is what I passed to the WaitForSeconds method. Proud of myself for this one. Perseverance wins.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Advertisements;
 
 /// <summary>
 /// This script counts the score,
@@ -27,16 +28,35 @@ public class GameManager : MonoBehaviour
     // Score text in the game over panel
     public Text gameOverScoreText;
 
+    // An array of audio clips for the slice sounds.
+    public AudioClip[] sliceSounds;
+    
+    // Audio clip for the fruit spawn sound
+    public AudioClip fruitSpawnSound;
+
+    // Audio clip for the bomb explosion.
+    public AudioClip bombExplosionSound;
+
+    // Audio source used to play sounds internally.
+    private AudioSource audioSource;
+
     /// <summary>
     /// When the game starts, display the high score.
     /// </summary>
     private void Awake()
     {
+        // Initializing Advertisements
+        // (For this to work, you need to pass in your Game Id as a string parameter)
+        //Advertisement.Initialize("");
+
         // Deactivate the Game Over Panel when the game starts
         gameOverPanel.SetActive(false);
 
         // Assign the HighScore to the HighScore Text when the game starts
         highScoreText.text = "HighScore: " + PlayerPrefs.GetInt("HighScore");
+
+        // Initializing Audio Source
+        audioSource = GetComponent<AudioSource>();
     }
 
     /// <summary>
@@ -65,14 +85,42 @@ public class GameManager : MonoBehaviour
     /// Method runs whenever the bomb is sliced. 
     /// Stops the game when a bomb is hit.
     /// </summary>
-    public void OnBombHit()
-    { 
-        Time.timeScale = 0;                 // This line of code stops the game.
+    public IEnumerator OnBombHit()
+    {
+        Debug.Log("Bomb Hit Called");
+        // Advertisement.Show();
+        Time.timeScale = 0.01f;                 // This line of code stops the game.
 
         // Set the score to the gameOverScoreText
         gameOverScoreText.text = "Score: " + score.ToString();
+        while (true)
+        {
+            yield return new WaitForSeconds(0.013f);
+            Debug.Log("Activate Panel");
+            gameOverPanel.SetActive(true);     // Activate the Game Over Panel when the game ends
+            Time.timeScale = 0f;                 // This line of code stops the game.
+            break;
+        }
+        
+    }
 
+    /// <summary>
+    /// Calls the Show Game Over Panel method with a delay of 1.3 seconds. 
+    /// (In time for the bomb explosion sound)
+    /// </summary>
+    public void InvokeGameOverPanel()
+    {
+        StartCoroutine(OnBombHit());
+        //Invoke("ShowGameOverPanel", 1.3f);
+    }
+
+    /// <summary>
+    /// This method shows the gameOver Panel.
+    /// </summary>
+    private void ShowGameOverPanel()
+    {
         gameOverPanel.SetActive(true);     // Activate the Game Over Panel when the game ends
+        Debug.Log("Show GameOver Panel");
     }
 
     /// <summary>
@@ -97,6 +145,39 @@ public class GameManager : MonoBehaviour
         {
             Destroy(gameObj);
         }
+    }
 
+    /// <summary>
+    /// Method used to play a random slice sound.
+    /// </summary>
+    public void PlayRandomSliceSound()
+    {
+        // Get a random audio clip from the sliceSounds array and play it
+        AudioClip randomSliceSound = sliceSounds[Random.Range(0, sliceSounds.Length)];
+        audioSource.PlayOneShot(randomSliceSound);
+    }
+
+    /// <summary>
+    /// This method plays the fruit spawn sound.
+    /// </summary>
+    public void PlayFruitSpawnSound()
+    {
+        if(fruitSpawnSound)
+        {
+            // Play the fruit spawn sound.
+            audioSource.PlayOneShot(fruitSpawnSound);
+        }
+    }
+
+    /// <summary>
+    /// This method plays the Bomb Explosion Sound
+    /// </summary>
+    public void PlayBombExplosionSound()
+    {
+        if (bombExplosionSound)
+        {
+            // Play the Bomb Explosion sound.
+            audioSource.PlayOneShot(bombExplosionSound);
+        }
     }
 }
